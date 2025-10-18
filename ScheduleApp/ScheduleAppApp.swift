2e88e194eda02d630fd82447a0536b401a518e58
Olesya Orlenko <olees.orlenko@yandex.ru>
2025-10-17 18:00:29 -0700

    Initial Commit

---
+++ ScheduleApp/ScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleAppApp.swift
+//  ScheduleApp
+//
+//  Created by Олеся Орленко on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
