eb8eecb40e6ab2328e0e5ecf8809221531ed61fd
Olesya Orlenko <olees.orlenko@yandex.ru>
2025-10-20 21:46:39 -0700

    feat: add copyright endpoint

---
+++ ScheduleApp/Services/CopyrightService.swift
@@ -0,0 +1,25 @@
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+typealias Copyright = Components.Schemas.CopyrightWrapper
+
+protocol CopyrightServiceProtocol {
+    func getCopyright() async throws -> Copyright
+}
+
+final class CopyrightService: CopyrightServiceProtocol {
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getCopyright() async throws -> Copyright {
+        let response = try await client.getCopyright(query: .init(
+            apikey: apikey
+        ))
+        return try response.ok.body.json
+    }
+}
--- ScheduleApp/Views/ContentView.swift
+++ ScheduleApp/Views/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  ScheduleApp
-//
-//  Created by Олеся Орленко on 17.10.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
